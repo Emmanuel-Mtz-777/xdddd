be6829910c440e9feeae993f7007bd147dbf6031
Leviek <hemmanuelmtz777@gmail.com>
2026-02-09 11:58:12 -0600

    third commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/staff/authentication', authRoutes);
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Mock API auth corriendo en http://localhost:${PORT}`);
 });
