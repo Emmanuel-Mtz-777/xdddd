ae108064a64c4bfabf679f2cfce0469bcef79901
Leviek <hemmanuelmtz777@gmail.com>
2026-02-09 12:04:56 -0600

    a

--- data/user.js
+++ data/user.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 
 const passwordPlain = '123456';
-const hash = await bcrypt.hash(passwordPlain, 10);
+const hash = bcrypt.hash(passwordPlain, 10);
 
 export const mockUser = {
   id: 1,
